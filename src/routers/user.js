6e33e25d5f3792aa36c12dd805363a4d692b926a
shyam <bhurgula.shyam@axiscades.com>
2020-04-12 22:53:05 +0530

    logout user added

--- src/routers/user.js
+++ src/routers/user.js
@@ -32,6 +32,28 @@ router.post('/users/login', async (req, res, next) => {
     }
 })
 
+router.post('/users/logout', auth, async (req, res, next) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => token.token != req.token)
+        await  req.user.save()
+        res.send('Loggedout Successfully')
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res, next) => {
+    try {
+        req.user.tokens = []
+         await  req.user.save()
+        res.send('Logged From All the Devices')
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 router.get('/users/me', auth, async (req, res, next) => {
     res.send(req.user)
 })
